882a677afd8629df125e09e324e2810b62707ee3
Tyler Nienhouse <tyler@nienhouse.net>
2016-04-02 17:41:52 -0700

    Start filling in functionality

--- js/jquery.cardtips.js
+++ js/jquery.cardtips.js
@@ -1,6 +1,9 @@
-(function($) {
+(function($, window, document) {
   var pluginName = 'cardtips',
       defaults = {
+        ajax: null,
+        align: 'bottom left',
+        debug: true,
         delayHide: 300,
         delayShow: 300,
       };
@@ -14,6 +17,10 @@
     this.$card;
     this.cardContent;
     this.hovered;
+    this.position = {
+      top:  0,
+      left: 0
+    };
 
     // start!
     this._init();
@@ -21,7 +28,23 @@
 
   Plugin.prototype = {
     _init: function() {
+      // disable the plugin on old browsers (including IE7 and lower)
+			if (document.querySelector) {
 
+        // save title
+        if (this.$target.data(pluginName + '-title') === undefined) {
+          var initialTitle = this.$target.attr('title');
+          if (initialTitle === undefined) initialTitle = null;
+          this.$target.data(pluginName + '-title', initialTitle);
+        }
+
+        this.$target
+          // remove title to prevent dual tooltips
+          .removeAttr('title')
+          // add the plugin target class
+          .addClass(pluginName + '-target');
+
+      } // if
     },
 
     _show: function() {
@@ -29,10 +52,18 @@
     },
 
     _unhide: function() {
-
+      this._createCard();
+      this._positionCard();
     },
 
     _hide: function() {
+
+    },
+
+    _createCard: function() {
+
+    },
+    _positionCard: function() {
 
     },
   }
@@ -46,9 +77,28 @@
     // when this method is called on a jQuery object
     } else {
 
+      // method calls are strings
+      if (typeof args[0] === 'string') {
+
+      // initializing (args[0] is undefined or we'll use as options object)
+      } else {
+
+        // dont add the tooltip if there's already one
+        if (this.hasClass(pluginName + '-target')) {
+          if (defaults.debug) console.log(pluginName + ': one or more cards have already been added to this element.');
+
+        // otherwise start the plugin on this
+        } else {
+          this.each(function() {
+            var instance = new Plugin(this, args[0]);
+            if (instance.options.debug) console.log(pluginName + ': Created new card instance.');
+          });
+        }
+
+      }
     }
 
     // allow chaining
     return this;
   }
-})(jQuery);
+})(jQuery, window, document);
